8675ada5e7fd97e7fe121b5216704b5e051ad050
thecarsgone <carolina.nicole.gonzalez@gmail.com>
2016-10-20 10:13:08 -0400

    changed path for master.csv

--- app/assets/javascripts/scatter.js
+++ app/assets/javascripts/scatter.js
@@ -3,7 +3,7 @@ var filename;
 var filename = $("#file").data("file");
 
 if (filename !== "") {
-  url = "/assets/" + filename; 
+  url = "/app/assets/" + filename; 
   $('.jumbotron h1').html(capitalizeEachWord(filename.replace(/\.\w*/,"").replace(/_/g," ")) + ", visualized");
 } else {
   url = "/assets/master.csv";
